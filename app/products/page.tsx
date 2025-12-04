ba4a6bf40c40e7bd6d2b6794f63b105d49c2e933
narawit <narawit.so@rmuti.ac.th>
2025-12-04 16:50:00 +0700

    test(home) contact button

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -73,8 +73,8 @@ export default function page() {
                     <button 
                     disabled = {currentPage === 1}
                     onClick={() => setCurrentInPage(p => Math.max(p - 1, 1))}
-                     className={`cursor-pointer p-1 rounded-md text-white 
-                            ${currentPage === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#249cff]'}
+                     className={` p-1 rounded-md text-white 
+                            ${currentPage === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#249cff] cursor-pointer'}
                         `}
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M15.488 4.43a.75.75 0 0 1 .081 1.058L9.988 12l5.581 6.512a.75.75 0 1 1-1.138.976l-6-7a.75.75 0 0 1 0-.976l6-7a.75.75 0 0 1 1.057-.081" clipRule="evenodd" /></svg>
@@ -100,8 +100,8 @@ export default function page() {
                     <button
                         disabled={currentPage === totalPage}
                         onClick={() => setCurrentInPage(p => Math.min(p + 1, totalPage))}
-                        className={`cursor-pointer p-1 rounded-md text-white 
-                            ${currentPage === totalPage ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#249cff]'}
+                        className={` p-1 rounded-md text-white 
+                            ${currentPage === totalPage ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#249cff] cursor-pointer'}
                         `}
                     >  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M8.512 4.43a.75.75 0 0 1 1.057.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.138-.976L14.012 12L8.431 5.488a.75.75 0 0 1 .08-1.057" clipRule="evenodd" /></svg>
                     </button>
